5462f515d0b9d659756e340d0bf708dbbcade454
Kritika Mishra <mishrakritika2001@gmail.com>
2023-01-30 12:09:25 +0530

    Added basic crud repository and extended it in like repository

--- src/repository/tweet-repository.js
+++ src/repository/tweet-repository.js
@@ -1,19 +1,16 @@
 //requiring tweet model here
 import Tweet from '../models/tweet.js'
+import CrudRepository  from './crud-repository.js';
 
-class TweetRepository{
+class TweetRepository extends CrudRepository{
+    //to use it here we need to make sure crudrepository's constructor is called
+    constructor(){
+        //calling super constructor with Tweet model
+        super(Tweet)
+    }
     async create(data){
         try{
             const tweet=await Tweet.create(data);
-            return tweet;
-        }catch(error){
-            console.log(error);
-        }
-    }
-
-    async get(id){
-        try{
-            const tweet=await Tweet.findById(id);
             return tweet;
         }catch(error){
             console.log(error);
@@ -32,14 +29,6 @@ class TweetRepository{
         }
     }
 
-    async destroy(id){
-        try{
-            const tweet=await Tweet.findByIdAndRemove(id);
-            return tweet;
-        }catch(error){
-            console.log(error);
-        }
-    }
 
     async getAll(offset, limit){
         try{
