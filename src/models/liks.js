3f0b79bdae727c5d96b0931cfac974765f72b588
Kritika Mishra <mishrakritika2001@gmail.com>
2023-01-30 11:43:36 +0530

    Added like model and basic user model

---
+++ src/models/liks.js
@@ -0,0 +1,26 @@
+import mongoose from 'mongoose';
+
+const likeSchema=new mongoose.Schema({
+    //on which model we are going to like
+    onModel:{
+        type: String,
+        required: true,
+        enum: ['Tweet','Comment']
+    },
+    //contains id of what model we liked it can be either tweetid or commentid
+    likeable: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        //it can refer to either Tweet or Comment model
+        refPath: 'onModel'
+    },
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    }
+},{timestamps:true}); 
+
+const Like=mongoose.model('Like',likeSchema);
+
+export default Like;
